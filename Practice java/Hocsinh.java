6580060686829f31846b9ba24000146036ee7f1c
triemnhn-2877 <99459263+triemnhn-2877@users.noreply.github.com>
2023-03-02 14:16:45 +0700

    nothing

---
+++ Practice java/Hocsinh.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class Hocsinh {
+	protected String name;
+	protected String address;
+	protected String age;
+	protected String i;
+	public String getI() {
+		return i;
+	}
+
+	public void setI(String i) {
+		this.i = i;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+	
+	public void themMoiHS() {
+		Hocsinh hs = new Hocsinh();
+		Scanner sc= new Scanner(System.in);
+		System.out.println("How many students wanna add ?");
+		i = sc.nextLine();
+		hs.setI(sc.nextLine());
+		System.out.println("Input name: ");
+		hs.setName(sc.nextLine());
+		System.out.println("Input age: ");
+		hs.setAge(sc.nextLine());
+		System.out.println("Input Address: ");
+		hs.setAddress(sc.nextLine());
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
